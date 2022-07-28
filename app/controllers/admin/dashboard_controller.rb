9bac632a4f9dbb01169c474e55a2cede38fa9563
Declan Davis <dexthefish@gmail.com>
2022-07-28 13:32:52 -0400

    add counts and links to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,9 @@ class Admin::DashboardController < ApplicationController
   if: -> { ENV["HTTP_BASIC_PASSWORD"].present? }
 
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
+
+# We could use AR models to query the database directly from the ERB views. However a better, more MVC approach to this is to let the controller fetch the data and pass it to the template. In other words, set instance variables in the controller action with values, and then render those instance variables in the ERB template
