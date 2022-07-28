24d62f42962f8a828213c8b779dcee20428d9129
Declan Davis <dexthefish@gmail.com>
2022-07-28 17:49:54 -0400

    routing for the About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
 
+  get "about", to: "about#index"
+
+
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -9,6 +12,8 @@ Rails.application.routes.draw do
     post   :add_item
     post   :remove_item
   end
+
+
 
   resources :orders, only: [:create, :show]
 
