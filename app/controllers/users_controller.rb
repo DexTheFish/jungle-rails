e23fbb13a3a0acf175f65023db3cbb8ff8f5f0ba
Declan Davis <dexthefish@gmail.com>
2022-07-29 19:11:29 -0400

    add user model, controller, and view for registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    raise user_params
     @user = User.new(user_params)
     @user.email.downcase!
     
     if @user.save
-      # If user saves in the db successfully:
       flash[:notice] = "Account created successfully!"
       redirect_to root_path
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-
   has_secure_password
 
-  # every user should have a unique email address
-  validates :email, presence: true, uniqueness: true
-
+	validates :email, presence: true, uniqueness: true
 end
